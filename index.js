32f3f13b8acc1c44b7c55a4da056e746668b8edb
ranjith-developer <114141106+ranjith-developer@users.noreply.github.com>
2022-09-26 16:07:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,29 @@ const initializeDBAndServer = async () => {
 };
 initializeDBAndServer();
 
+// Get Books API With Query Parameters
+
+app.get("/books/", async (request, response) => {
+  const {
+    offset = 2,
+    limit = 5,
+    order = "ASC",
+    order_by = "book_id",
+    search_q = "",
+  } = request.query;
+  const getBooksQuery = `
+    SELECT
+      *
+    FROM
+     book
+    WHERE
+     title LIKE '%${search_q}%'
+    ORDER BY ${order_by} ${order}
+    LIMIT ${limit} OFFSET ${offset};`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
+
 // Get Books API
 app.get("/books/", async (request, response) => {
   const getBooksQuery = `
